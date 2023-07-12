c0361ba3f2ac0cdb54a3fc7d91900b81b59d4bb5
gabriel M <gabriel_jm_3@hotmail.com>
2023-07-12 17:52:15 -0300

    dfsdf

--- main.js
+++ main.js
@@ -5,11 +5,8 @@ var typed = new Typed('#element', {
 });
 
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 4,
-  spaceBetween: 30,
-  centeredSlides: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
