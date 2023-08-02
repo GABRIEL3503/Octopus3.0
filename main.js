cb8d42c6497381b6680e47463cb178fa8bc1fa4d
gabriel M <gabriel_jm_3@hotmail.com>
2023-08-02 19:05:38 -0300

    gabowinnwer

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+window.addEventListener("scroll", function() {
+  var navbar = document.getElementById("navbar");
+
+  if (window.pageYOffset > 0) {
+    navbar.classList.remove("transparent");
+    navbar.classList.add("gradient");
+  } else {
+    navbar.classList.remove("gradient");
+    navbar.classList.add("transparent");
+  }
+});
+
+// Asegúrate de que el navbar sea transparente al inicio
+document.getElementById("navbar").classList.add("transparent");
+
+
 var typed = new Typed('#element', {
   strings: ['Atractivos.', 'Personsalizados' ,'Veloces.'],
   typeSpeed: 111,
@@ -62,7 +78,12 @@ var iconElement = document.getElementById('toggleIcon');
 var buttons = document.querySelectorAll('button');
 var links = document.querySelectorAll('#navbar ul li a');
 var liH2Elements = document.querySelectorAll('li h2'); // selecciona todos los h2 dentro de un li
-var cardH2aElements = document.querySelectorAll('.card .h2a'); // selecciona todos los .h2a dentro de .card
+var h2aElement = document.querySelectorAll('.h2a');
+var cardosElements = document.querySelectorAll('.cardo');
+var titulosH2Elements = document.querySelectorAll('.titulos h2:first-of-type');
+var bn39spanElements = document.querySelectorAll('.bn39span');
+var navbarElement = document.querySelector('#navbar.gradient');
+
 
 
 // Funciones para cambiar los temas
@@ -71,11 +92,12 @@ function applyTheme(themeName) {
   h1Element.className = themeName + '-theme-h1';
   h2Element.className = themeName + '-theme-h2';
   iconElement.className = themeName + '-theme-icon';
+  h2aElement.className = themeName + '-theme-h2a';
   liH2Elements.forEach(function(h2) {
     h2.className = themeName + '-theme-lih2';
   });
-  cardH2aElements.forEach(function(h2a) {
-    h2a.className = themeName + '-theme-card-h2a';
+  cardosElements.forEach(function(element) {
+    element.className = 'cardo ' + themeName + '-theme-cardo';
   });
   
   buttons.forEach(function(button) {
@@ -85,7 +107,27 @@ function applyTheme(themeName) {
   links.forEach(function(link) {
     link.className = themeName + '-theme-link';
   });
+
+  
+  h2aElement.forEach(function(h2a) {
+    h2a.className = themeName + '-theme-h2a';
+  });
+
+  titulosH2Elements.forEach(function(element) {
+    element.className = themeName + '-theme-titulos-h2-after';
+  });
+  bn39spanElements.forEach(function(element) {
+    // Primero, elimina cualquier clase de tema anterior
+    element.classList.remove('light-theme-bn39span', 'dark-theme-bn39span', 'colorful-theme-bn39span');
+    // Luego, agrega la nueva clase de tema
+    element.classList.add(themeName + '-theme-bn39span');
+  });
+  navbarElement.classList.remove('light-theme-navbar', 'dark-theme-navbar', 'colorful-theme-navbar');
+  // Agrega la nueva clase de tema
+  navbarElement.classList.add(themeName + '-theme-navbar');
 }
+
+
 
 function applyLightTheme() {
   applyTheme('light');
@@ -105,3 +147,6 @@ document.getElementById('dark-theme-button').addEventListener('click', applyDark
 document.getElementById('colorful-theme-button').addEventListener('click', applyColorfulTheme);
 
 applyDarkTheme();
+
+
+
